2c51047f813023988030c36bd240380bbb6da45e
Max Forssén <max.forssen@mentimeter.com>
2025-06-05 17:21:23 +0200

    Fix routing: remove duplicate monkeybusiness101 path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://monkeybusiness101.github.io",
-	base: "/monkeybusiness101",
+	site: "https://monkeybusiness101.com",
+	base: "/",
 });
